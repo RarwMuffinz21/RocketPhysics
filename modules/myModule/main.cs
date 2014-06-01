2bea5a8c60c7adcfb7a90ff59e568201fc3e88e6
RarwMuffinz2 <Rarw.Muffinz21@gmail.com>
2014-06-01 16:14:01 -0700

    InitUpload

---
+++ modules/myModule/main.cs
@@ -0,0 +1,27 @@
+function myModule::create(%this)
+{
+	exec("./gui/guiprofiles.cs");
+	exec("./scripts/scenewindow.cs");
+	exec("./scripts/scene.cs");
+	exec("./scripts/background.cs");
+	exec("./scripts/spaceship.cs");
+	exec("./scripts/asteroids.cs");
+	exec("./scripts/controls.cs");
+	createSceneWindow();
+	createScene();
+	mySceneWindow.setScene(myScene);
+	createBackground();
+	createSpaceShip();
+	createAsteroids(20);
+	new ScriptObject(InputManager);
+	mySceneWindow.addInputListener(InputManager);
+	InputManager.Init_controls();
+}
+
+
+function MyModule::destroy(%this)
+{
+	shipcontrols.pop();
+	destroySceneWindow();
+	InputManager.delete();
+}
